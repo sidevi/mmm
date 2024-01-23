e35f4d392daaf5e0e87edaa13d92f46f02b28056
okosun <okosunmicheal123@gmail.com>
2024-01-22 22:22:27 -0700

    first c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using PasswordGenerator;
+
+// Console.WriteLine("my name is chales");
+// var name = Console.ReadLine();
+// var currentDate = DateTime.Now;
+// Console.WriteLine($"{Environment.NewLine}Hello,{name},on {currentDate:d}");
+// Console.Write($"{Environment.NewLine}press any key to exit");
+// Console.ReadKey(true);
+
+
+var pwd = new Password();
+var password = pwd.Next();
+
+
+Console.WriteLine(password);
